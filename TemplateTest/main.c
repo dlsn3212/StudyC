797d5e144852925a5efed300a42bf509389c8705
yujin <dywjd2341@naver.com>
2020-01-31 11:35:51 +0900

    2장 완료

--- TemplateTest/main.c
+++ TemplateTest/main.c
@@ -7,11 +7,14 @@
 
 
 int main() {
-	printf("Be happy\n");
-	printf("1234567890\n");
-	printf("My\tfriend\n");
-	printf("Goot\bd\tchance\n");
-	printf("Cow\rW\a\n");
+	printf("%d\n",10);
+	printf("%1f\n",3.4);
+	printf("%.1lf\n",3.45);
+	printf("%.10lf\n",3.4);
+	printf("%d과 %d의 합은 %d입니다.\n",10,20,10+20);
+	printf("%.1lf-%.1lf = %.1f\n", 3.4, 1.2, 3.4 - 1.2);
     
+	//printf("%d-%d", 3.4, 1.2);
+	//오류
 	return 0;
 }
