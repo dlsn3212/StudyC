c2a086474e2e8eca374db59a2b47fd9e81c65ada
yujin <dywjd2341@naver.com>
2020-02-03 10:40:08 +0900

    4장완료

--- Operation/main.c
+++ Operation/main.c
@@ -13,8 +13,17 @@
 // 메인함수
 int main(void) 
 {
-	printf("Hello World!\n");
-    // type here.
+    int a = 10;
+    int b = 12;
+
+    printf("a & b : %d \n", a & b);
+    printf("a ^ b : %d \n", a ^ b);
+    printf("a | b : %d \n", a | b);
+    printf("~a : %d \n", ~a);
+    printf("a << 1 : %d \n", a << 1);
+    printf("a >> 2 : %d \n", a >> 2);
+
+
 	system("pause");
 	return EXIT_SUCCESS;
 }
